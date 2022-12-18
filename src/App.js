60ed0ae5d5ed3bfed99325b76ac310013876f5cb
alsubogd <alsousha@mail.ru>
2022-12-18 13:35:31 +0200

    hl1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import Header from  './components/header/Header'
-import Main from  './components/main/Main'
-import Footer from './components/footer/Footer';
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import Footer from "./components/footer/Footer";
 
-function App(){
-    //return JSX -> return ()
-    return (
-        <div className='root__wrapper'>
-            <Header />
-            <Main />
-            <Footer />
-        </div>
-    )
-    
-  }
+function App() {
+  //return JSX -> return ()
+  //New commit
+  return (
+    <div className="root__wrapper">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
 
-  export default App;
-  
+export default App;
